a87fa9b785be2d85bed174d4261c30a0bd5fe2c3
Tayan21 <bostybaev@gmail.com>
2023-01-23 17:16:31 +0600

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  if(n < 0) {
+    n = n * (-1)
+  }
+  let res = 0;
+  while(n > 0) {
+    res *= 10
+    res = res + (n % 10)
+    n = Math.trunc(n / 10)
+  }
+
+  return res;
 }
